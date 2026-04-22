03d2c6dac9023f192abd1c4c15500668695db178
I_Swerk_I <I_Swerk_I@DESKTOP-1LE6UQ2>
2026-04-22 20:00:15 +1000

    Add project files.

---
+++ SequenxeContainers/main.cpp
@@ -0,0 +1,136 @@
+#include <iostream>
+#include <array>
+#include <vector>
+#include<deque>
+#include<list>
+#include<forward_list>
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define tab "\t"
+#define delimeter "\n----------------------------------\n"
+
+template <typename T>
+void vector_properties(const std::vector<T>& vec)
+{
+	cout << "Size:\t" << vec.size() << endl;
+	cout << "Capacity: " << vec.capacity() << endl;
+	cout << "MaxSize: " << vec.max_size() << endl;
+}
+bool less_then_10(const int value)
+{
+	return value < 10;
+}
+
+//#define STL_ARRAY
+//#define STL_VECTOR
+//#define STL_DEQUE
+//#define STL_LIST
+#define STL_FORWARD_LIST
+
+void main()
+{
+	setlocale(LC_ALL, "");
+#ifdef STL_ARRAY
+	std::array<int, 5> arr = {3,5,8,13,21};
+	for (int i = 0; i < arr.size(); i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+#endif // STL_ARRAY
+
+#ifdef STL_VECTOR
+	std::vector<int> i_vec = {0,1,1,2,3,5,8,13,21,34};
+	vector_properties(i_vec);
+	i_vec.push_back(55);
+	i_vec.push_back(89);
+	vector_properties(i_vec);
+	i_vec.shrink_to_fit();
+	int value;
+	double index;
+	cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	cout << "Введите значение добавляемого элемента: "; cin >> value;
+	if(index<=i_vec.size())
+		i_vec.insert(i_vec.begin() + index, value);
+
+	for (std::vector<int>::iterator it = i_vec.begin(); it != i_vec.end(); ++it)
+	{
+		cout << *it << endl;
+	}
+	cout << endl;
+	cout << "Front:\n" << i_vec.front();
+	cout << "Back:\n" << i_vec.back()<<endl;
+	
+#endif // STL_VECTOR
+
+#ifdef STL_DEQUE
+	std::deque<int> i_deque = { 3,5,8,13,21 };
+	for (int i = 0; i < i_deque.size(); i++)
+	{
+		cout << i_deque[i] << tab;
+	}
+	cout << endl;
+	i_deque.push_front(1);
+	i_deque.push_front(1);
+	i_deque.push_front(0);
+	for (std::deque<int>::iterator it = i_deque.begin(); it != i_deque.end(); ++it)
+		cout << *it << tab;
+	cout << endl;
+#endif // STL_DEQUE
+
+#ifdef STL_LIST
+	std::list<int> i_list = { 3,5,8,13,21 };
+	int index;
+	int value;
+	cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	cout << "Введите значение добавляемого элемента: "; cin >> value;
+	std::list<int>::iterator position = i_list.begin();
+	std::advance(position, index);
+	i_list.insert(position, value);
+	
+	
+	for (std::list<int>::iterator it = i_list.begin(); it != i_list.end(); ++it)
+		cout << *it << tab;
+	cout<<endl;
+
+	for (std::list<int>::reverse_iterator it = i_list.rbegin(); it != i_list.rend(); ++it)
+		cout << *it << tab;
+	cout << endl;
+
+#endif // STL_LIST
+
+#ifdef STL_FORWARD_LIST
+	std::forward_list<int> i_flist = { 3,5,8,13,21 };
+	for (std::forward_list<int>::iterator it = i_flist.begin(); it != i_flist.end(); ++it)
+	{
+		cout << *it << tab;
+	}
+	cout << endl;
+	cout << "Размер списка: " << std::distance(i_flist.begin(), i_flist.end()) << endl;
+	int index;
+	int value;
+	cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	cout << "Введите значение добавляемого элемента: "; cin >> value;
+	std::forward_list<int>::iterator position = i_flist.begin();
+	//for (int i = 0; i < index-1; i++)position++;
+	/*if (index > 0 && index < std::distance(i_flist.begin(), i_flist.end()))
+	{
+		std::advance(position, index - 1);
+		i_flist.insert_after(position, value);
+		
+	}*/
+	//i_flist.remove(13);
+	i_flist.remove_if(less_then_10);
+	for (std::forward_list<int>::iterator it = i_flist.begin(); it != i_flist.end(); ++it)
+	{
+		cout << *it << tab;
+	}
+	cout << endl;
+
+#endif // FORWARD_LIST
+
+
+
+}
